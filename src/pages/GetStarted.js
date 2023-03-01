e585a0a384afc912762c4e3505082f59db7db7ad
andyyparkk <seminpar@usc.edu>
2023-03-01 14:01:42 -0800

    finished adding submission of email for all types

--- src/pages/GetStarted.js
+++ src/pages/GetStarted.js
@@ -66,14 +66,34 @@ export default function GetStarted(props) {
                 <div className="h-screen min-h-[600px] justify-center items-center flex px-[14px]">
                     <div className="w-full flex flex-col items-center font-poppins h-[300px]">
                         <h1 className="gradient-text-blue font-semibold text-[28px] text-center leading-tight mb-6">The Phynite Marketplace<br/>is Coming Soon</h1>
-                        <h2 className="font-[500] text-white text-[15px] text-center mb-6">Sign up to be the first to know when we launch.</h2>
-                        <form onSubmit={onSubmit} className="flex border-2 border-gray1 duration-100 rounded-xl w-full">
-                            <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="h-12 w-full rounded-l-xl text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
-                            <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue duration-300 rounded-r-xl font-poppins text-gray1 text-lg font-semibold">Sign Up</button>
-                        </form>
-                        {error !== null &&(
-                            <h1 className="text-md pt-3 font-poppins text-salmon px-1 text-center">*{error}</h1>
+                        {submitted === true ? (
+                            <div>
+                                <div className="">
+                                    <h2 className="font-[500] text-lg text-white text-center mb-5">Thank you for being one of our earliest supporters!</h2>
+                                    <h2 className="text-gray-400 text-center mb-6">Phynite will be built before you know it.</h2>
+                                    <div className="flex justify-center">
+                                        <button onClick={()=>setSubmitted(false)} className=" underline text-gray0 text-xs text-center">Add another email</button>
+                                    </div>
+                                </div>               
+                            </div>
+                        ):(
+                            <div>
+                                <h2 className="font-[500] text-white text-[15px] text-center mb-6">Sign up to be the first to know when we launch.</h2>
+                                <form onSubmit={onSubmit} className="flex border-2 border-gray1 duration-100 rounded-xl w-full">
+                                    <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="h-12 w-full rounded-l-xl text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
+                                    {loading === true ? (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-salmon to-mango brightness-125 rounded-r-lg flex items-center justify-center"><ClipLoader size={20} color={"#ffffff"} /></button>
+                                    ) : (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue duration-300 rounded-r-xl font-poppins text-gray1 text-lg font-semibold">Sign Up</button>
+                                    )}
+                                        
+                                </form>
+                                {error !== null &&(
+                                    <h1 className="text-md pt-3 font-poppins text-salmon px-1 text-center">*{error}</h1>
+                                )}        
+                            </div>
                         )}
+                        
                     </div>
                     
                 </div>
@@ -82,28 +102,62 @@ export default function GetStarted(props) {
                 <div className="px-[18px] w-full h-screen min-h-[600px] justify-center items-center flex">
                     <div className="w-full flex flex-col items-center font-poppins h-[300px]">
                         <h1 className="gradient-text-blue font-semibold text-4xl text-center leading-tight mb-6">The Phynite Marketplace<br/>is Coming Soon</h1>
-                        <h2 className="font-[500] text-white text-lg text-center mb-6">Sign up to be the first to know when we launch.</h2>
-                        <form onSubmit={onSubmit} className="flex border-2 border-gray1 duration-100 rounded-xl w-full max-w-[500px]">
-                            <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="h-12 w-full rounded-l-xl text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
-                            <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue duration-300 rounded-r-xl font-poppins text-gray1 text-lg font-semibold">Sign Up</button>
-                        </form>
-                        {error !== null &&(
-                            <h1 className="text-md pt-3 font-poppins text-salmon px-1 text-center">*{error}</h1>
+                        {submitted === true ? (
+                            <div className="">
+                                <h2 className="font-[500] text-xl text-white text-center mb-5">Thank you for being one of our earliest supporters!</h2>
+                                <h2 className="text-lg text-gray-400 text-center mb-6">Phynite will be built before you know it.</h2>
+                                <div className="flex justify-center">
+                                    <button onClick={()=>setSubmitted(false)} className=" underline text-gray0 text-sm text-center">Add another email</button>
+                                </div>
+                            </div>   
+                        ): (
+                            <div>
+                                <h2 className="font-[500] text-white text-lg text-center mb-6">Sign up to be the first to know when we launch.</h2>
+                                <form onSubmit={onSubmit} className="flex border-2 border-gray1 duration-100 rounded-xl w-full max-w-[500px]">
+                                    <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="h-12 w-full rounded-l-xl text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
+                                    {loading === true ? (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-salmon to-mango brightness-125 rounded-r-lg flex items-center justify-center"><ClipLoader size={20} color={"#ffffff"} /></button>
+                                    ) : (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue duration-300 rounded-r-xl font-poppins text-gray1 text-lg font-semibold">Sign Up</button>
+                                    )}
+                                        
+                                </form>
+                                {error !== null &&(
+                                    <h1 className="text-md pt-3 font-poppins text-salmon px-1 text-center">*{error}</h1>
+                                )}
+                            </div>
                         )}
+                        
                     </div>
                 </div>
             )}
             {props.displayType === "smallComputer" && (
                 <div className="h-screen min-h-[600px] justify-center items-center flex">
                     <div className="w-full flex flex-col items-center font-poppins h-[300px]">
-                        <h1 className="gradient-text-blue font-semibold text-5xl text-center leading-tight mb-6">The Phynite Marketplace<br/>is Coming Soon</h1>
-                        <h2 className="font-[500] text-xl text-white text-center mb-6">Sign up to be the first to know when we launch.</h2>
-                        <form onSubmit={onSubmit} className="flex border-2 border-gray1 hover:border-gray0 duration-100 rounded-lg">
-                            <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="w-[400px] h-12 rounded-l-lg text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
-                            <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue hover:brightness-125  duration-300 rounded-r-lg font-poppins text-gray1 text-lg font-semibold hover:from-salmon hover:to-mango">Sign Up</button>
-                        </form>
-                        {error !== null &&(
-                            <h1 className="text-md pt-3 font-poppins text-salmon w-[500px] text-center">*{error}</h1>
+                        <h1 className="gradient-text-blue font-semibold text-5xl text-center leading-tight mb-6">The Phynite Marketplace<br />is Coming Soon</h1>
+                        {submitted === true ? (
+                            <div className="">
+                                <h2 className="font-[500] text-2xl text-white text-center mb-5">Thank you for being one of our earliest supporters!</h2>
+                                <h2 className="text-xl text-gray-400 text-center mb-6">Phynite will be built before you know it.</h2>
+                                <div className="flex justify-center">
+                                    <button onClick={()=>setSubmitted(false)} className=" underline text-gray0 text-center">Add another email</button>
+                                </div>
+                            </div>   
+                        ) : (
+                            <div className="">
+                                <h2 className="font-[500] text-xl text-white text-center mb-6">Sign up to be the first to know when we launch.</h2>
+                                <form onSubmit={onSubmit} className="flex border-2 border-gray1 hover:border-gray0 duration-100 rounded-lg">
+                                    <input value={userInfo.email} onChange={handleChange} name="email" type="text" className="w-[400px] h-12 rounded-l-lg text-lg bg-gray2 pl-4 focus:ring-1 text-white focus:ring-gray0 focus:outline-none placeholder:text-gray-400" placeholder="Email address"></input>
+                                    {loading === true ? (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-salmon to-mango brightness-125 rounded-r-lg flex items-center justify-center"><ClipLoader size={20} color={"#ffffff"} /></button>
+                                    ) : (
+                                        <button type="submit" className="w-[120px] h-12 bg-gradient-to-br from-phyniteBlue to-phyniteBlue hover:brightness-125  duration-300 rounded-r-lg font-poppins text-gray1 text-lg font-semibold hover:from-salmon hover:to-mango">Sign Up</button>
+                                    )}
+                                </form>
+                                {error !== null &&(
+                                    <h1 className="text-md pt-3 font-poppins text-salmon w-[500px] text-center">*{error}</h1>
+                                )}
+                            </div>
                         )}
                     </div>
                     
