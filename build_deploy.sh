f413f4fbbe37743179775aa40b56e88bfc122559
Hongbum Park <keenchin@gmail.com>
2023-03-09 23:49:27 -0800

    Change build_deploy.sh for uploding files to s3

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,15 +1,3 @@
 rm -r -f ./build
 npm run build
-cp -R ./deploy/* ./build
-
-aws deploy push --application-name landing-frontend \
-    --description "landing-frontend upload" \
-    --ignore-hidden-files \
-    --s3-location s3://mintdeploy/landing-frontend.zip \
-    --source ./build
-
-aws deploy create-deployment \
-  --application-name landing-frontend \
-  --deployment-config-name CodeDeployDefault.OneAtATime \
-  --deployment-group-name landing-frontend \
-  --s3-location bucket=mintdeploy,bundleType=zip,key=landing-frontend.zip
+aws s3 sync ./build s3://landing-frontend --acl public-read
