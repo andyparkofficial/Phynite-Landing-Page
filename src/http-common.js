55a4798c39223e77dd38b4d6c7ad6118e7e177ca
Andy Park <andyparkofficial@gmail.com>
2023-08-11 17:49:18 -0700

    update

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ axios.defaults.withCredentials = false;
 // if false, cross-site requests cannot include cookies or authorization headers
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://www.phynite.io/api/v1",
   headers: {
     "Content-type": "application/json",
   },
